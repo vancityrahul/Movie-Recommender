dde3073a393e11799dcf0abd7bc0d3b18ab1b5a2
Rahul <vancityrahul7@gmail.com>
2024-09-25 10:16:31 +0530

    Completed tests for processor.py

--- backend/tests/unit/test_processor.py
+++ backend/tests/unit/test_processor.py
@@ -1,6 +1,5 @@
 import pytest
 from mock import patch
-import numpy as np
 from numpy.ma.core import array
 from backend.core.processor import Processor
 from backend.tests.data.sample import data
@@ -18,8 +17,24 @@ def mock_client_search():
 @pytest.mark.asyncio
 class Testprocessor:
 
-    async def test_processor_exception(self) -> None:
-        pass
+    async def test_processor_exception(self, mock_embeddings) -> None:
+        mock_embeddings.side_effect = Exception("Test exception")
+        query = "Test_Query"
+        genre = ["Test"]
+        k = 3
+        with pytest.raises(Exception) as exc_info:
+            response = await Processor.process(query=query, genre=genre, k=k)
+        assert str(exc_info.value) == "Test exception"
+
+    async def test_processor_v2_exception(self) -> None:
+        query = "Test_Query"
+        k = 3
+        with patch('backend.core.qclient.Config.client.search') as mock_search:
+            mock_search.side_effect = Exception("Test exception")
+
+            with pytest.raises(Exception) as exc_info:
+                response = await Processor.process_v2(query=query, k=k)
+            assert str(exc_info.value) == "Test exception"
 
     async def test_process(self, mock_embeddings, mock_client_search) -> None:
         mock_embeddings.return_value = embd = {
@@ -34,6 +49,24 @@ class Testprocessor:
         response  = await Processor.process(query = query,
                                             genre = genre,
                                             k = k)
+
+        assert response == [
+                              "Alatriste",
+                              "America Is Still the Place",
+                              "American Hero"
+                            ]
+
+    async def test_process_v2(self, mock_embeddings, mock_client_search) -> None:
+        mock_embeddings.return_value = embd = {
+                "one_hot_embeddings": array([1,0,0,0,0,0]),
+                "hf_embeddings": array([0,1,12,3,4,234,423,34])
+            }
+        # vectors = np.concatenate([embd["one_hot_embeddings"], embd["hf_embeddings"]])
+        mock_client_search.return_value = data
+        query = "Test_Query"
+        k = 3
+        response  = await Processor.process_v2(query=query,
+                                            k=k)
 
         assert response == [
                               "Alatriste",
